97da2f34d9dbe49d8704cdca9fefb42fa73480db
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-22 17:07:18 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 164 ms, Memory: 14776 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		int[][] apt = new int[15][15];
+		
+		for(int i = 0; i < 15; i++) {
+			apt[i][1] = 1;
+			apt[0][i] = i;
+		}
+		for(int i = 1; i < 15; i++) {
+			for(int j = 2; j< 15; j ++) {
+				apt[i][j] = apt[i][j-1] + apt[i-1][j];
+			}
+		}
+		for(int i = 0; i < t; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			bw.write(apt[k][n] + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
