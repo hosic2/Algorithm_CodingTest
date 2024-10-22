49f6fd1cea26696cc23ba89d766b55331ca630c8
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-22 18:18:13 +0900

    Update update.py

--- update.py
+++ update.py
@@ -49,7 +49,7 @@ def main():
 
         for file in files:
             if category not in solveds:
-                content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+                content += "|{}|[문제 보러가기]({})|\n".format(category, parse.quote(os.path.join(root, file)))
                 solveds.append(category)
                 print("category : " + category)
 
