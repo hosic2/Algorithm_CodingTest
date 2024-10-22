f23f462bb3628754a42cbbc2bb5876d0d17a5890
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-22 18:20:20 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,17 +3,16 @@
 import os
 from urllib import parse
 
-HEADER="""# 
+HEADER = """# 
 # 백준 & 프로그래머스 문제 풀이 목록
-
 """
 
 def main():
     content = ""
     content += HEADER
     
-    directories = [];
-    solveds = [];
+    directories = []
+    solveds = []
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
@@ -38,6 +37,9 @@ def main():
         if directory not in directories:
             if directory in ["백준", "프로그래머스"]:
                 content += "## 📚 {}\n".format(directory)
+                content += "<details>\n"  # 시작: 토글 기능
+                content += "<summary>문제 목록 보기</summary>\n"  # 요약 텍스트
+                content += "\n"  # 요약과 문제 목록 사이의 빈 줄
             else:
                 if directory in [0, 1, 2, 3, 4, 5, '0', '1', '2', '3', '4', '5']:
                     content += "### 🚀 Lv{}\n".format(directory)
@@ -49,9 +51,13 @@ def main():
 
         for file in files:
             if category not in solveds:
-                content += "|{}|[문제 보러가기]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+                content += "| {} | [문제 보러가기]({}) |\n".format(category, parse.quote(os.path.join(root, file)))
                 solveds.append(category)
                 print("category : " + category)
+
+        # 토글 기능 종료
+        if directory in ["백준", "프로그래머스"]:
+            content += "</details>\n"  # 종료: 토글 기능
 
     with open("README.md", "w") as fd:
         fd.write(content)
