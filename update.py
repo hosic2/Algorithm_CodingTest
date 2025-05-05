2f6c4d1f4fbc1c14d4bcfdc8c301edfa6f0cc69b
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-05 21:01:02 +0900

    Update update.py
    - 새로운 풀이 저장되도록 변경
    - 테스트

--- update.py
+++ update.py
@@ -1,19 +1,16 @@
-#!/usr/bin/env python
-
 import os
 from urllib import parse
 
-HEADER="""# 
+HEADER = """# 
 # 백준 & 프로그래머스 문제 풀이 목록
-
 """
 
 def main():
     content = ""
     content += HEADER
     
-    directories = [];
-    solveds = [];
+    directories = []
+    solveds = []
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
@@ -49,12 +46,29 @@ def main():
 
         for file in files:
             if category not in solveds:
-                content += "|{}|[문제 보러가기]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+                # 문제 번호 폴더 경로
+                problem_folder = os.path.join(root, category)
+                
+                # 문제 폴더가 존재하지 않으면 생성
+                if not os.path.exists(problem_folder):
+                    os.makedirs(problem_folder)
+                
+                # 풀이가 2번째부터는 하위 폴더를 생성
+                solution_index = len([name for name in os.listdir(problem_folder) if os.path.isdir(os.path.join(problem_folder, name))]) + 1
+                solution_folder = os.path.join(problem_folder, f"풀이{solution_index}")
+                
+                # 풀이 하위 폴더가 없으면 생성
+                if not os.path.exists(solution_folder):
+                    os.makedirs(solution_folder)
+
+                # 파일 경로 생성
+                file_path = os.path.join(solution_folder, file)
+                content += "|{}|[문제 보러가기]({})|\n".format(category, parse.quote(file_path))
                 solveds.append(category)
                 print("category : " + category)
 
     with open("README.md", "w") as fd:
         fd.write(content)
-        
+
 if __name__ == "__main__":
     main()
