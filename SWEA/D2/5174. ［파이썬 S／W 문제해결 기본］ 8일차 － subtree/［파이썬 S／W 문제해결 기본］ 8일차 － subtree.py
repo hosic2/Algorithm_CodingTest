9d6f67e965bc5b249c6f05bf88ea1055fb65a68a
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-06 23:42:22 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 8일차 - subtree, Time: 65 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
+++ SWEA/D2/5174. ［파이썬 S／W 문제해결 기본］ 8일차 － subtree/［파이썬 S／W 문제해결 기본］ 8일차 － subtree.py
@@ -1,12 +1,24 @@
 t = int(input())
 
 for i in range(t):
-    n, m = map(int, input().split())
+    e, n = map(int, input().split())
     nums = list(map(int, input().split()))
-    answer = [m]
-    
+    tree = {}
+
+    # 트리 구성: 부모를 키로, 자식 리스트를 값으로
     for j in range(0, len(nums), 2):
-        if nums[j] in answer:
-            answer.append(nums[j + 1])
-            
-    print(f'#{i + 1}', len(answer))
+        p, c = nums[j], nums[j + 1]
+        if p in tree:
+            tree[p].append(c)
+        else:
+            tree[p] = [c]
+
+    # DFS 정의
+    def dfs(node):
+        count = 1  # 자기 자신
+        if node in tree:
+            for child in tree[node]:
+                count += dfs(child)
+        return count
+
+    print(f'#{i + 1}', dfs(n))
