a9dcd3ac614095ede4b401d2c8f5d911a21e888e
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-06 16:50:36 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 3일차 - 회문, Time: 102 ms, Memory: 60,032 KB -BaekjoonHub

---
+++ SWEA/D2/4861. ［파이썬 S／W 문제해결 기본］ 3일차 － 회문/［파이썬 S／W 문제해결 기본］ 3일차 － 회문.py
@@ -0,0 +1,24 @@
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    answer = ''
+    # 세로 회문 확인을 위해 한번에 다 받기
+    word = [input() for _ in range(n)]
+    
+    for num in range(n):
+    	# 가로 확인
+        for j in range(n - m + 1):
+            temp = word[num][j : j + m]
+            if temp == temp[::-1]:
+                answer = temp
+                break
+                
+         # 세로 확인
+        for j in range(n - m + 1):
+            temp = ''.join(word[j + k][num] for k in range(m))
+            if temp == temp[::-1]:
+                answer = temp
+                break
+                
+    print(f'#{i + 1}', answer)
