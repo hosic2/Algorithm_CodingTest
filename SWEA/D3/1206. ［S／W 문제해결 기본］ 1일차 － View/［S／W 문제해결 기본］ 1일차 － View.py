6c6617fb70d32475e7f30d6493171d11b56bfe02
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-07 18:36:36 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 73 ms, Memory: 59,648 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -3,8 +3,9 @@ for i in range(10):
     a = list(map(int, input().split()))
     answer = 0
 
-    for j in range(2, len(a) - 2):
-        if a[j] > a[j - 1] and a[j] > a[j + 1] and a[j] > a[j - 2] and a[j] > a[j + 2]:
-            answer += a[j] - max(a[j + 1], a[j - 1], a[j + 2], a[j - 2])
+    for j in range(2, n - 2):
+        around = [a[j - 2], a[j - 1], a[j + 1], a[j + 2]]
+        if a[j] > max(around):
+            answer += a[j] - max(around)
 
     print(f'#{i + 1}', answer)
