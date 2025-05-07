2f281247b6426c43bbd7a4e054ff353e990dde0b
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-07 20:45:44 +0900

    [D1] Title: 평균값 구하기, Time: 57 ms, Memory: 52,224 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
@@ -3,4 +3,5 @@ t = int(input())
 for i in range(t):
     arr = list(map(int, input().split()))
     
-    print(f'#{i + 1}', round(sum(arr) / len(arr)))
+    # 파이썬 사사오입 방지 -> 그냥 round 테스트 했는데 통과해버림 ;
+    print(f'#{i + 1}', int(sum(arr) / len(arr) + 0.5))
