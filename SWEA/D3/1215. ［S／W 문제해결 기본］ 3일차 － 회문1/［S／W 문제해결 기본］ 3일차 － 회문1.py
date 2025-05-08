d5db312cb7514a53fc3a4b1cae9d75d6775f5a5d
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-08 20:23:53 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 69 ms, Memory: 59,136 KB -BaekjoonHub

---
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -0,0 +1,15 @@
+for k in range(1, 11):
+    n = int(input())
+    arr = [input() for _ in range(8)]
+    answer = 0
+    
+    for i in range(8):
+        for j in range(8 - n + 1):
+            if arr[i][j : j + n] == arr[i][j : j + n][::-1]:
+                answer += 1
+            
+            col = ''.join(arr[x][i] for x in range(j, j + n))
+            if col == col[::-1]:
+                answer += 1
+                
+    print(f'#{k}', answer)
