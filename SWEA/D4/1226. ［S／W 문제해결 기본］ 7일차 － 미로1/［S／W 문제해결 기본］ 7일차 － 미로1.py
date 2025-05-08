d59868fe570b99f3ba189beae4acd3b91ad7a09d
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-08 21:47:20 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 69 ms, Memory: 59,648 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,26 +1,29 @@
 def dfs(x, y):
-    global ans
+    global answer
+    # 시작점은 방문처리
     arr[x][y] = 1
+    # 상 하 좌 우
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    # 상하좌우 모두 체크
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
         if arr[nx][ny] == 3:
-            ans = 1
+            answer = 1
             return
 
         if 0 <= nx < 16 and 0 <= ny < 16 and arr[nx][ny] == 0:
-            arr[nx][ny] = 1
             dfs(nx, ny)
 
-for i in range(1, 11):
-
-    # 0: 길, 2: 출발점, 3: 도착점
+for _ in range(10):
     n = int(input())
     arr = [list(map(int, input())) for _ in range(16)]
-    ans = 0
+    answer = 0
 
+    # 시작점 호출
     dfs(1, 1)
-    print(f'#{n}', 1 if ans else 0)
+
+    print(f'#{n}', answer)
