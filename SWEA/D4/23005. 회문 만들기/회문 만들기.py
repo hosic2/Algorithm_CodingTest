2d3a3c78c26532e4c029070e5b4f702c2047b8f7
주호 <70103964+hosic2@users.noreply.github.com>
2025-05-10 22:16:04 +0900

    [D4] Title: 회문 만들기, Time: 125 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D4/23005. 회문 만들기/회문 만들기.py
+++ SWEA/D4/23005. 회문 만들기/회문 만들기.py
@@ -1,13 +1,9 @@
-def min_insertions_to_make_palindrome(s: str) -> int:
-    # 1. 이미 회문이면
+def calc(s):
     if s == s[::-1]:
         return 0
-
-    # 2. x가 없고 회문도 아니면
     if 'x' not in s:
         return -1
 
-    # 3. 투 포인터 접근
     left, right = 0, len(s) - 1
     cnt = 0
 
@@ -22,11 +18,11 @@ def min_insertions_to_make_palindrome(s: str) -> int:
             right -= 1
             cnt += 1
         else:
-            return -1  # 중간에 x를 삽입해도 맞출 수 없음
+            return -1
 
     return cnt
 
 T = int(input())
 for _ in range(T):
     s = input().strip()
-    print(min_insertions_to_make_palindrome(s))
+    print(calc(s))
